bf1bfd72c35eaafaa997975b86fcd195f35aa8de
Sushil N. Raut <sushilraut20@gmail.com>
2018-04-03 16:14:17 -0700

    Update gradientDescent.m

--- Linear Regression/gradientDescent.m
+++ Linear Regression/gradientDescent.m
@@ -18,10 +18,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    
-    %delta = (1/m)*sum(X.*repmat((X*theta - y), 1, size(X,2)));
-    
-    %theta = (theta' - (alpha * delta))';
+   
     
     theta1=theta(1);
     theta2=theta(2);
